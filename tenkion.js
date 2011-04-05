e92995ce5b5e7fd4a00bb5c2a83e86f503c587d3
AQ <aq@okaq.com>
2011-04-05 18:17:41 -0400

    Explicit static file data and load to memory in Tenkion.

--- tenkion.js
+++ tenkion.js
@@ -11,17 +11,43 @@ var tenkion = {
 		this.http = require('http');
 		this.fs = require('fs');
 		this.url = require('url');
+
+		// static_files
+		this.niles = [
+			new nile(nile.a[0], nile.b[0], nile.b[5]),
+			new nile(nile.a[1], nile.b[0], nile.b[3]),
+			new nile(nile.a[2], nile.b[0], nile.b[5]),
+			new nile(nile.a[3], nile.b[0], nile.b[3]),
+			new nile(nile.a[4], nile.b[0], nile.b[5]),
+			new nile(nile.a[5], nile.b[0], nile.b[5])
+		];
+		this.load();
+	},
+	"load": function() {
+		for (var i = 0; i < this.niles.length; i++) {
+			this.fs.readFile(
+				this.niles[i].url.slice(1),
+				this.niles[i].enc,
+				function(err, data) {
+					if (err) {
+						console.log("fs.readFile arror reading file: " + this.niles[i].url + ". Error message: " + err);
+						this.niles[i].fd = "";
+					}
+					this.niles[i].fd = data;
+				}
+			);	
+		}
 	}
 }
 
-function nile(e0, u0, h0) {
+function nile(u0, e0, h0) {
 	this.enc = e0;
 	this.url = u0;
 	this.fd = undefined;
-	this.header = h0;
+	this.hdr = h0;
 }
 
-nile.prototype.a = [
+nile.prototype.b = [
 	"utf-8",
 	"gzip",
 	"text/plain",
@@ -30,13 +56,13 @@ nile.prototype.a = [
 	"text/javascript"
 ];
 
-nile.prototype.b = [
+nile.prototype.a = [
 	"/ab/okaq.js",
 	"/ab/okaq.html",
 	"/ab/motd.js",
 	"/ab/speed.html",
 	"/ab/speed.js",
 	"/ab/leek.js"
-];
+]; // nextus: use fs.readdir()
 
 
